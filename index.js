65845c36c7f2b22990855435c37e5d25ee52e1d4
Даниил <daniil.axiyan.316@mail.ru>
2021-11-15 08:57:51 +0300

    хрень

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 "use strict"
+/* function check() {
+    var submit = document.getElementsByName('submit')[0];
+    if (document.getElementById('politics').checked){
+        
+    else
+        submit.disabled = 'disabled';
+    } */
