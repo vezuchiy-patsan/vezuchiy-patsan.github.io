4fe354e8c6b115ac66f34bc2adb0f313f6541e5b
Даниил <daniil.axiyan.316@mail.ru>
2021-12-04 00:38:16 +0300

    действующая регистрация

--- server.php
+++ server.php
@@ -1,22 +1,34 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    ini_set('error_reporting', E_ALL);
+
     if (isset($_POST['login'])) 
     { $login = $_POST['login']; 
-        if ($login == '') { unset($login);
-        } 
-    } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем 
+     
+        if ($login == '') { 
+           
+            unset($login);
+        } else{
+           
+        }
+    } 
+    //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем 
     if (isset($_POST['password'])) { 
-        $password=$_POST['password']; 
+        $password=$_POST['password'];
         if ($password =='') { 
+        
             unset($password);
         } 
     }
     //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем 
     if (empty($login) or empty($password)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем
     {
-        ob_start();
+        /* ob_start();
         $new_url = 'http://akhiyan.ml.ml/index.html';
         header('Location: '.$new_url);
-        ob_end_flush();
+        ob_end_flush(); */
+        exit ('Лох');
     }
     //если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
     $login = stripslashes($login);
@@ -26,25 +38,32 @@
     //удаляем лишние пробелы
     $login = trim($login);
     $password = trim($password);
+    print $login;
+    print $password;
     //подключение бд
+    
     require("connect.php");
+
+    if ($conn === false) {
+        die("Ошибка: " . mysqli_connect_error());
+      }else{ 
+      echo "Подключение успешно установлено";};
     //проверим нет ли такого же пользователя
+    
     $test_data = mysqli_query($conn, "SELECT id FROM log_Account WHERE login='$login'");
+
     $result = mysqli_fetch_array($test_data);
     if(!empty($result['id'])){
-        exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+        exit("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
     };
+    echo 'pidor';
     //сохраняем новые данные
-    $resultOk = mysqli_query($conn, "INSERT INTO log_Account (login,password) VALUES('$login','$password')");
-
-    //проверим на ошибки
-    if($resultOK == 'TRUE'){
-        ob_start();
-        $new_url = 'https://i.ytimg.com/vi/fGem9bbVFAM/maxresdefault.jpg';
-        header('Location: '.$new_url);
-        ob_end_flush();
-    }else{
-        echo "Бляяяяяяяяяяяяяяяяяяяяяяяя!!!";
+    $sql = "INSERT INTO log_Account (id, login, pass, accountID) VALUES (1,'$login','$password', 1)";
+    
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно добавлены";
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
     }
 
 ?>
